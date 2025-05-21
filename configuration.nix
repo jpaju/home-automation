5dd151a411be421c24560e84e388c2414aef612d
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2025-05-21 20:12:57 +0300

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,105 @@
+{ config, pkgs, username, userhome, email, ... }: {
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  virtualisation.vmware.guest.enable = true;
+
+  time.timeZone = "Europe/Helsinki";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    #   font = "Lat2-Terminus16";
+    keyMap = "fi";
+    #   useXkbConfig = true; # use xkb.options in tty.
+  };
+
+  # Don't remove if shells are installed by other means, otherwise nix environment is not loaded correctly
+  programs = {
+    fish.enable = true;
+    zsh.enable = true;
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  users.users."${username}" = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    shell = pkgs.fish;
+
+    openssh.authorizedKeys.keys =
+      [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILNCVK2DGtkXqX5eN+yGgBf7uDddLr89PCSYmIUhfobJ ${email}" ];
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+
+  sops = {
+    defaultSopsFile = ./secrets.yaml;
+    defaultSopsFormat = "yaml";
+    age.keyFile = "${userhome}/.config/sops/age/keys.txt"; # TODO User different filepath?
+
+    secrets.cloudflare_api_token = { };
+  };
+
+  environment.systemPackages = with pkgs; [ git sops ];
+
+  networking.hostName = "home-automation";
+  networking.networkmanager.enable = true;
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [ 22 80 443 8080 ];
+
+  services.openssh = {
+    enable = true;
+    settings.PermitRootLogin = "no";
+    settings.AcceptEnv = "TERM COLORTERM";
+  };
+
+  security.acme = {
+    acceptTerms = true;
+
+    defaults = {
+      inherit email;
+      dnsProvider = "cloudflare";
+      credentialFiles.CF_DNS_API_TOKEN_FILE = config.sops.secrets."cloudflare_api_token".path;
+    };
+  };
+
+  services.nginx = {
+    enable = true;
+
+    virtualHosts = let
+      dockerUrl = "http://docker.int.jpaju.fi";
+      proxyTo = backendUrl: {
+        locations."/" = {
+          proxyPass = backendUrl;
+          proxyWebsockets = true;
+          recommendedProxySettings = true;
+        };
+
+        # TODO Replace enableSSL with forceSSL?
+        addSSL = true;
+        enableACME = true;
+        acmeRoot = null;
+      };
+    in {
+      "hass.jpaju.fi" = proxyTo "${dockerUrl}:8123";
+      "esphome.int.jpaju.fi" = proxyTo "${dockerUrl}:6052";
+      "zigbee2mqtt.int.jpaju.fi" = proxyTo "${dockerUrl}:8080";
+      "zwavejs.int.jpaju.fi" = proxyTo "${dockerUrl}:8091";
+      "portainer.int.jpaju.fi" = proxyTo "${dockerUrl}:9000";
+    };
+  };
+
+  virtualisation.docker.enable = true;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  system.stateVersion = "24.11"; # DO NOT CHANGE THIS
+
+}
+
---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "My NixOS configuration as a flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    dotfiles = {
+      url = "github:jpaju/dotfiles/expose-nix-modules";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, sops-nix, dotfiles, ... }:
+    let
+      system = "x86_64-linux";
+      username = "jaakko";
+      userhome = "/home/${username}";
+      email = "jaakko.paju2@gmail.com";
+      specialArgs = {
+        inherit system username userhome email;
+        fishUtils = dotfiles.homeManagerModules.fishUtils;
+        scls = dotfiles.inputs.scls;
+        helix = dotfiles.inputs.helix;
+      };
+    in {
+      nixosConfigurations.home-automation = nixpkgs.lib.nixosSystem {
+        inherit system specialArgs;
+
+        modules = [
+          ./configuration.nix
+          ./hardware-configuration.nix
+
+          sops-nix.nixosModules.sops
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = specialArgs;
+
+            home-manager.users.${username} = { pkgs, ... }: {
+              home.stateVersion = "25.05";
+
+              programs.home-manager.enable = true;
+
+              home.username = username;
+              home.homeDirectory = "/home/${username}";
+
+              home.packages = with pkgs; [ tree fzf ];
+
+              imports = with dotfiles.homeManagerModules; [ fish git gh starship helix zellij ];
+            };
+          }
+        ];
+      };
+    };
+}
+
