538369f51dcf178d22f6b61ceffd6e18dbcef1e8
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2025-05-25 23:54:19 +0300

    nix: automatically run GC every week

--- system/nix-settings.nix
+++ system/nix-settings.nix
@@ -1,6 +1,13 @@
 { ... }: {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
 
   # Don't remove if shells are installed by other means, otherwise nix environment is not loaded correctly
   programs = {
